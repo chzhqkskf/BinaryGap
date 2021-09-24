6e1126991c932060e082005fb08fc268c712865b
JJ <chzhqkskf@gmail.com>
2021-09-24 17:39:38 +0900

    First Commit

--- main.py
+++ main.py
@@ -4,31 +4,24 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(N):
-    b_n = bin(N)
-    print(b_n)
+def func(A, K):
+    tmp = list(range(len(A)))
+    for i in range(len(A)):
+        tmp[(i+K)%len(A)] = A[i]
 
-    tmp = []
-    for i in range(2, len(b_n)):
-        print(b_n[i])
-        if b_n[i] == '1':
-            tmp.append(i)
+    return tmp
 
-    print(tmp)
 
-    result = [0]
-    for i in range(len(tmp)-1):
-        result.append(tmp[i+1]-tmp[i]-1)
-
-    return max(result)
 
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    A = [3, 8, 9, 7, 6]
+    K = 3
 
-    N = 32
-    print(print_hi(N))
+    func(A, K)
+
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
